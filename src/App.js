aac949fa29ecf8d9a975b71f571a0f28b4582f75
Scott Harvey <scottyharvey33@gmail.com>
2021-09-23 12:01:12 -0600

    main - update button use State

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import React, { useState } from 'react'
 import './App.css';
 
 function App() {
+
+  const [state, setState] = useState('')
+
   return (
     <div className="App">
       <h1>Naruto</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet perferendis ex obcaecati blanditiis ut cum dignissimos eum autem id incidunt iste, fugiat quam laboriosam adipisci tenetur temporibus qui ipsum quidem?</p>
+      <button onClick={() => setState('Dude')}>CLICKED {state}</button>
     </div>
   );
 }
